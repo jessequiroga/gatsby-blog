3cd6ea54cbc40fde27823f9e863feae6e7ad30ca
dan make <danmakes2022@gmail.com>
2019-01-26 17:54:24 +0000

    rename blog to stash and blogpost to post

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  const postTemplate = path.resolve(`./src/templates/blog-post.js`)
   return graphql(
     `
       {
@@ -39,7 +39,7 @@ exports.createPages = ({ graphql, actions }) => {
 
       createPage({
         path: post.node.fields.slug,
-        component: blogPost,
+        component: postTemplate,
         context: {
           slug: post.node.fields.slug,
           previous,
