ef344853934f0a0f5ae23c2501552cae034f0cb8
dan make <danmakes2022@gmail.com>
2019-01-27 17:46:09 +0000

    update override theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -10,9 +10,11 @@ lincolnTheme.overrideThemeStyles = () => {
         'linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0) 1px, #f7b733 1px, #f7b733 2px, rgba(0, 0, 0, 0) 2px)',
     },
     blockquote: { borderLeft: '0.5925rem solid #f7b733' },
+    '@media only screen and (max-width: 480px){blockquote{backgroundImage:0.29625rem solid #134896}}': {},
     'h1 a, h2 a, h3 a, h4 a, h5 a, h6 a': {
       backgroundImage: `none`,
     },
+    h4: { lineHeight: 1.3 },
     'ul li': { marginBottom: 0 },
   }
 }
