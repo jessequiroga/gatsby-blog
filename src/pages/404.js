88603b7ba478b901feed0ecfa8192fbe5932eb01
dan make <danmakes2022@gmail.com>
2019-01-27 09:41:47 +0000

    define site title for 4040

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,7 @@ import { Layout, SEO } from '../components/index'
 class NotFoundPage extends React.Component {
   render() {
     const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
